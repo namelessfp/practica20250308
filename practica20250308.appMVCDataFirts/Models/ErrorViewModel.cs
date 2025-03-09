21ac53155346dc5dabd5b2c6450015f977ad4059
bayro <bayro@JOERI>
2025-03-08 23:42:25 -0600

    Agregar archivos de proyecto.

---
+++ practica20250308.appMVCDataFirts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practica20250308.appMVCDataFirts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
